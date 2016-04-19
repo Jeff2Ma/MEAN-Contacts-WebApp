2bea50559bce51afbe871f99db0d053559fbac6f
Jeff2Ma <mjh2316@gmail.com>
2016-04-20 00:23:47 +0800

    add update,edit,clean functions.

--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 A simple Contact List Web App build with MEAN( MongoDB, Express, Angular.js, Node.js).
 
 Just for practices, have fun :-)
+
+# Start
+
+Before starting, make sure that your enviroment has installed Node.js and MongoDB.
+
+1. Clone this rep.
+
+2. `cd` to folder `MEAN-Contacts-WebApp`.
+
+3. Run `npm install`.
+
+4. Start node server by using `node app.js` and open `http://localhost:3000/`.
+
+# Thanks
+
+Inspired by those [videos](https://www.youtube.com/playlist?list=PLX2HoWE32I8Nkzw2TqcifObuhgJZz8a0U).
--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/contactlist', function (req, res) {
     console.log("I received a GET request");
 
     db.contactlist.find(function (err, docs) {
-        console.log(docs);
+        //console.log(docs);
         res.json(docs);
     });
 
@@ -40,12 +40,37 @@ app.get('/contactlist', function (req, res) {
 
 
 app.post('/contactlist', function (req, res) {
-    console.log(req.body);
+    //console.log(req.body);
     db.contactlist.insert(req.body, function (err, doc) {
         res.json(doc);
     });
 });
 
+app.delete('/contactlist/:id', function (req, res) {
+    // console.log(id);
+    db.contactlist.remove({_id: mongojs.ObjectID(id)}, function (err, doc){
+        res.json(doc);
+    });
+    var id = req.params.id;
+});
+
+app.get('/contactlist/:id', function (req, res) {
+    var id = req.params.id;
+    // console.log(id);
+    db.contactlist.findOne({_id: mongojs.ObjectID(id)}, function (err, doc) {
+        res.json(doc);
+    });
+});
+
+app.put('/contactlist/:id', function (req, res) {
+    var id = req.params.id;
+    console.log(req.body.name);
+    db.contactlist.findAndModify({query: {_id: mongojs.ObjectID(id)},
+        update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+        new: true}, function (err, doc) {
+        res.json(doc);
+    });
+});
 
 app.listen(3000);
 console.log("running!");
